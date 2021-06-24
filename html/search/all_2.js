46d6383ee38f2c63a07f22209f75976b657130ec
Doxygen CI <ci-arduino@invalid>
2021-06-24 18:04:59 +0000

    Deploy docs to GitHub Pages from commit 6af029123b
    Commit: 6af029123b80a55b35eb3faeab40604b8eb0abf8
    GitHub Actions run: 968893289

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['mcp3008_5fspi_5ffreq',['MCP3008_SPI_FREQ',['../_adafruit___m_c_p3008_8h.html#a4c071628164c9dde94b41cde4152e43d',1,'Adafruit_MCP3008.h']]],
   ['mcp3008_5fspi_5fmax',['MCP3008_SPI_MAX',['../_adafruit___m_c_p3008_8h.html#a589ae31195f5e35b29d886679c8e7edc',1,'Adafruit_MCP3008.h']]],
   ['mcp3008_5fspi_5fmax_5f3v',['MCP3008_SPI_MAX_3V',['../_adafruit___m_c_p3008_8h.html#ab5c391f56f1fa9907e0156734d4f044c',1,'Adafruit_MCP3008.h']]],
   ['mcp3008_5fspi_5fmax_5f5v',['MCP3008_SPI_MAX_5V',['../_adafruit___m_c_p3008_8h.html#a468b3739fd693bfa5619eba0b72e907b',1,'Adafruit_MCP3008.h']]],
